326ae9306a466620ab2c873481d28571db650cfb
Nelreina <nelreina@gmail.com>
2018-04-03 02:12:11 -0400

    feature/logout client logout OK!

--- src/client/views/Protected.js
+++ src/client/views/Protected.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { translate } from 'react-i18next';
+import { connect } from 'react-redux';
+import { authActions as actions } from 'nelreina-web-utils';
 
-const Protected = ({ t }) => {
-  return <div>{t('Protected')}</div>;
+const Protected = ({ t, logout }) => {
+  return (
+    <div>
+      <h3>{t('Protected')}</h3>
+      <button onClick={logout} className="btn btn-danger">
+        Log Out
+      </button>
+    </div>
+  );
 };
 
-export default translate()(Protected);
+export default connect(null, actions)(translate()(Protected));
